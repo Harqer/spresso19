1d3b2c8b3b386bd721275fc57473820ca3776b3d
Harqer <aoolllathens@outlook.com>
2026-09-17 20:05:24 -0400

    style(chat): normalize message imports

--- composeApp/src/commonMain/kotlin/components/features/chat/widgets/ChatMessageItem.kt
+++ composeApp/src/commonMain/kotlin/components/features/chat/widgets/ChatMessageItem.kt
@@ -2,9 +2,9 @@ package components.features.chat.widgets
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
